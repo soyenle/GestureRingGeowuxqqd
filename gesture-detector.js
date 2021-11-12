46ca7e5dc24ae127e3cd292d72064186bf42f666
soyenle <34003821+soyenle@users.noreply.github.com>
2021-11-12 14:12:12 +0100

    Update gesture-detector.js

--- gesture-detector.js
+++ gesture-detector.js
@@ -68,3 +68,98 @@ AFRAME.registerComponent('gesture-detector', {
             this.el.emit(eventName, currentState);
 
             this.internalState.previousState = currentState;
+        }
+
+        if (gestureContinues) {
+            const eventDetail = {
+                positionChange: {
+                    x: currentState.position.x - previousState.position.x,
+
+                    y: currentState.position.y - previousState.position.y,
+                },
+            };
+
+            if (currentState.spread) {
+                eventDetail.spreadChange = currentState.spread - previousState.spread;
+            }
+
+            // Update state with new data
+
+            Object.assign(previousState, currentState);
+
+            // Add state data to event detail
+
+            Object.assign(eventDetail, previousState);
+
+            const eventName =
+                `${this.getEventPrefix(currentState.touchCount)}fingermove`;
+
+            this.el.emit(eventName, eventDetail);
+        }
+    },
+
+    getTouchState(event) {
+        if (event.touches.length === 0) {
+            return null;
+        }
+
+        // Convert event.touches to an array so we can use reduce
+
+        const touchList = [];
+
+        for (let i = 0; i < event.touches.length; i++) {
+            touchList.push(event.touches[i]);
+        }
+
+        const touchState = {
+            touchCount: touchList.length,
+        };
+
+        // Calculate center of all current touches
+
+        const centerPositionRawX =
+            touchList.reduce((sum, touch) => sum + touch.clientX, 0) /
+            touchList.length;
+
+        const centerPositionRawY =
+            touchList.reduce((sum, touch) => sum + touch.clientY, 0) /
+            touchList.length;
+
+        touchState.positionRaw = { x: centerPositionRawX, y: centerPositionRawY };
+
+        // Scale touch position and spread by average of window dimensions
+
+        const screenScale = 2 / (window.innerWidth + window.innerHeight);
+
+        touchState.position = {
+            x: centerPositionRawX * screenScale,
+            y: centerPositionRawY * screenScale,
+        };
+
+        // Calculate average spread of touches from the center point
+
+        if (touchList.length >= 2) {
+            const spread =
+                touchList.reduce((sum, touch) => {
+                    const value = (
+                        sum +
+                        Math.sqrt(
+                            Math.pow(centerPositionRawX - touch.clientX, 2) +
+                            Math.pow(centerPositionRawY - touch.clientY, 2)
+                        )
+                    );
+                    return value;
+                }, 0) / touchList.length;
+
+            touchState.spread = spread * screenScale;
+        }
+
+        return touchState;
+    },
+
+    getEventPrefix(touchCount) {
+        const numberNames = ['one', 'two', 'three', 'many'];
+
+        return numberNames[Math.min(touchCount, 4) - 1];
+    },
+});
